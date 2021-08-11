a7476e9087ca8e9d195de27c0211dfdb23825d56
azat <marat_az@mail.ru>
2021-08-11 15:15:25 +0600

    added closed function

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path("delete-todo/<id>", delete_todo, name="delete-todo"),
     path("mark-todo/<id>", mark_todo, name="mark-todo"),
     path("unmark-todo/<id>", unmark_todo, name="unmark-todo"),
+    path("close-todo/<id>", close_todo, name="close-todo"),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
